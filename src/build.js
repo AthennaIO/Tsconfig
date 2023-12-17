2ddf08ec9229b7e20775997cf0476d7435e34f0d
jlenon7 <lenonSec7@gmail.com>
2023-12-17 12:25:16 +0000

    config(ts): add emitDecoratorMetadata option to true

--- src/build.js
+++ src/build.js
@@ -7,23 +7,22 @@
  * file that was distributed with this source code.
  */
 
-
-import { tsc } from './tsc.js'
-import { rimraf } from 'rimraf'
-import { copyfiles } from './copyfiles.js'
-import { META_FILES, TS_CONFIG_PATH, BUILD_FOLDER_NAME } from './constants.js'
+import { tsc } from "./tsc.js";
+import { rimraf } from "rimraf";
+import { copyfiles } from "./copyfiles.js";
+import { META_FILES, TS_CONFIG_PATH, BUILD_FOLDER_NAME } from "./constants.js";
 
 /**
  * Delete old `BUILD_FOLDER_NAME` folder.
  */
-await rimraf(BUILD_FOLDER_NAME)
+await rimraf(BUILD_FOLDER_NAME);
 
 /**
  * Compile the application using tsc.
  */
-await tsc(TS_CONFIG_PATH)
+await tsc(TS_CONFIG_PATH);
 
 /**
  * Copy `META_FILES` to `BUILD_FOLDER_NAME` folder.
  */
-await copyfiles(META_FILES, BUILD_FOLDER_NAME)
+await copyfiles(META_FILES, BUILD_FOLDER_NAME);
--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,33 @@
 /**
+ * @athenna/tsconfig
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ *
  * Files that will be copied to the `BUILD_FOLDER_NAME` folder.
  */
 export const META_FILES = [
-  'templates/**/*',
-  'configurer/**/*',
-  'resources/**/*',
-  'package.json',
-  'package-lock.json',
-  'LICENSE.md',
-  'README.md'
-]
+  "templates/**/*",
+  "configurer/**/*",
+  "resources/**/*",
+  "package.json",
+  "package-lock.json",
+  "LICENSE.md",
+  "README.md",
+];
 
 /**
  * Path to tsconfig file that will be used by `tsc` compiler.
  */
-export const TS_CONFIG_PATH = 'node_modules/@athenna/tsconfig/tsconfig.lib-build.json'
+export const TS_CONFIG_PATH =
+  "node_modules/@athenna/tsconfig/tsconfig.lib-build.json";
 
 /**
  * Build folder name that files will be copied to.
  */
-export const BUILD_FOLDER_NAME = 'build'
+export const BUILD_FOLDER_NAME = "build";
